89e92a1740f0543c095709f6b29596b6e42f5ad9
Gilson Roberto dos Santos <124139853+GIR0SAN@users.noreply.github.com>
2023-06-18 14:49:34 -0300

    Add files via upload
    Page I created for a security company in the city where I live.

---
+++ mobile-navbar.js
@@ -0,0 +1,52 @@
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+      this.mobileMenu = document.querySelector(mobileMenu);
+      this.navList = document.querySelector(navList);
+      this.navLinks = document.querySelectorAll(navLinks);
+      this.activeClass = "active";
+  
+      this.handleClick = this.handleClick.bind(this);
+    }
+  
+    animateLinks() {
+      this.navLinks.forEach((link, index) => {
+        link.style.animation
+          ? (link.style.animation = "")
+          : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+              index / 7 + 0.3
+            }s`);
+      });
+    }
+  
+    handleClick() {
+      this.navList.classList.toggle(this.activeClass);
+      this.mobileMenu.classList.toggle(this.activeClass);
+      this.animateLinks();
+    }
+  
+    addClickEvent() {
+      this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+  
+    init() {
+      if (this.mobileMenu) {
+        this.addClickEvent();
+      }
+      return this;
+    }
+  }
+  
+  const mobileNavbar = new MobileNavbar(
+    ".l-hamburguer__wrapper",
+    ".l-nav__menu",
+    ".c-menu__link",
+  );
+  mobileNavbar.init();
+  
+  var $doc = $('html, body');
+  $('.scrollSuave').click(function() {
+     $doc.animate({
+         scrollTop:$($.attr(this,'href')).offset().top
+     }, 500);        
+     return false;
+ });
